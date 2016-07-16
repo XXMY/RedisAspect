868adb9b084354d1c9d4cd70b74a1ce98b4cef25
caifw <caifw@wasu.com>
2016-07-16 09:23:19 +0800

    Finish resid hash data operation in basically.

---
+++ src/cfw/reflect/ReflectConsts.java
@@ -0,0 +1,7 @@
+package cfw.reflect;
+
+/**
+ * Created by Cfw on 2016/7/16.
+ */
+public class ReflectConsts {
+}
---
+++ src/test/Main.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by Cfw on 2016/7/16.
+ */
+public class Main {
+}
